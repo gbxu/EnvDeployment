28df7bfb5a169e97c8644e0533e06baa57aa0c7b
gbxu <gb.xu@outlook.com>
2025-03-18 23:54:14 +0800

    update

--- ubuntu/2_register_ssh.sh
+++ ubuntu/2_register_ssh.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
 THIS_PATH=$(readlink -f "$0")
 THIS_DIR=$(dirname "$THIS_PATH")
 MY_ENV_DEPLOYMENT=${THIS_DIR}/../../EnvDeployment
@@ -10,6 +9,7 @@ fi
 if [ -d "${HOME}/.ssh" ]; then
     echo "${HOME}/.ssh exists."
     # 复制认证
+    echo "Register your authorized_keys to ${HOME}/.ssh/authorized_keys"
     if [ -f "${HOME}/.ssh/authorized_keys" ]; then
         cat ${MY_ENV_DEPLOYMENT}/configs/.ssh/authorized_keys >> ${HOME}/.ssh/authorized_keys
     else
@@ -17,14 +17,21 @@ if [ -d "${HOME}/.ssh" ]; then
     fi
     # 复制公钥
     if [ ! -f "${HOME}/.ssh/id_rsa.pub" ]; then
+        echo "Register your id_rsa.pub to ${HOME}/.ssh/id_rsa.pub"
         cp ${MY_ENV_DEPLOYMENT}/configs/.ssh/id_rsa.pub ${HOME}/.ssh/
+    else
+        echo "${HOME}/.ssh/id_rsa.pub exists."
     fi
     # 复制配置
     if [ ! -f "${HOME}/.ssh/config" ]; then
+        echo "Register your config to ${HOME}/.ssh/config"
         cp ${MY_ENV_DEPLOYMENT}/configs/.ssh/config ${HOME}/.ssh/
+    else
+        echo "${HOME}/.ssh/config exists."
     fi
 else
     # 复制整个文件夹
+    echo "Register your .ssh to ${HOME}/.ssh"
     cp -r ${MY_ENV_DEPLOYMENT}/configs/.ssh ${HOME}/.ssh
 fi
 
@@ -34,6 +41,9 @@ chmod 644 ${HOME}/.ssh/id_rsa.pub
 
 # 复制私钥
 if [ ! -f "${HOME}/.ssh/id_rsa" ]; then
+    echo "Register your id_rsa to ${HOME}/.ssh/id_rsa"
     cp ${MY_ENV_DEPLOYMENT}/privatedata/.ssh/id_rsa ${HOME}/.ssh/
+else
+    echo "${HOME}/.ssh/id_rsa exists."
 fi
 chmod 600 ${HOME}/.ssh/id_rsa
